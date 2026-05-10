e5420b2c997620438d0c8f249f1a0e833d5d9e12
chr0nzz <187675356@users.noreply.github.com>
2026-05-10 01:45:12 -0400

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://xyzlab.dev',
   output: 'static',
+  integrations: [sitemap()],
   server: {
     host: '0.0.0.0',
     port: 4321,
